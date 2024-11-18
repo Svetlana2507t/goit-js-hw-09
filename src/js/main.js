e594aed4209915173cb3a530ffebc1415b770503
Svitlana Markevych <svetlana2507t@gmail.com>
2024-11-18 21:10:55 +0100

    to build

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { markup } from "./1-gallery.js";
+import { formData } from "./2-form.js";
--- src/main.js
+++
@@ -1,4 +0,0 @@
-console.log("hello world");
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { markup } from "./js/1-gallery.js";
-import { formData } from "./js/2-form.js";
