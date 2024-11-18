845f591e986785a7df6f04835a87d97ec4e924f5
Svetlana2507t <104763350+Svetlana2507t@users.noreply.github.com>
2024-11-18 10:22:26 +0000

    Deploying to gh-pages from @ Svetlana2507t/goit-js-hw-09@259783661424eac686f0a85d2cf38b32c9a9f7a1 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/1-gallery-Cxuoo0g5.js";/* empty css                      */import"./assets/vendor-CgTBfC_f.js";console.log("hello world");
+import"./assets/1-gallery-Cxuoo0g5.js";/* empty css                      */import"./assets/vendor-CgTBfC_f.js";const e=document.querySelector("feedback-form");e.addEventListener("input",handleInput);const o=e.elements.email;console.log(o);const t=e.elements.message;console.log(t);console.log("hello world");
 //# sourceMappingURL=index.js.map
