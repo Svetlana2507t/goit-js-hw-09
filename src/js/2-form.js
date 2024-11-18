2525c9616871ec51b28e8dce0bffea958963942b
Svitlana Markevych <svetlana2507t@gmail.com>
2024-11-18 11:40:21 +0100

    log values to Console

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,15 +6,15 @@ export const formData = {
 const form = document.querySelector("feedback-form");
 form.addEventListener("input", handleInput);
 const email = form.elements.email;
-console.log(email);
+console.log(`email: ${email}`);
 const textarea = form.elements.message;
-console.log(textarea);
+console.log(`textarea.value: ${textarea.value}`);
 
 const messageInput = function handleInput(event) {
   formData.email = email.value;
   formData.message = message.value;
   localStorage.add(formData);
-  console.log(formData);
+  console.log(`formData: ${formData}`);
 
   return formData;
 };
