f5a50a0cc46025ab6544996dbc5c3dc4510eab04
Svetlana2507t <104763350+Svetlana2507t@users.noreply.github.com>
2024-11-18 20:11:36 +0000

    Deploying to gh-pages from @ Svetlana2507t/goit-js-hw-09@e594aed4209915173cb3a530ffebc1415b770503 🚀

--- index.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-UvXNCwUC.js";import"./assets/vendor-CgTBfC_f.js";import"./assets/1-gallery-BSIhSqSc.js";import"./assets/2-form-BOfzjcww.js";console.log("hello world");
-//# sourceMappingURL=index.js.map
